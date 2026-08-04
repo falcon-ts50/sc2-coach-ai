c1c665d9a5358ff2eef225341400c6c8a9e4a114
falcon-ts50 <53598733+falcon-ts50@users.noreply.github.com>
2026-08-04 20:39:26 +0300

    Add portal context test

---
+++ java/portal/src/test/java/ai/sc2coach/portal/Sc2CoachPortalApplicationTest.java
@@ -0,0 +1,11 @@
+package ai.sc2coach.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sc2CoachPortalApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
