faa2f3952ae33ddcc78e7be05cb346c79d15dd6d
falcon-ts50 <53598733+falcon-ts50@users.noreply.github.com>
2026-08-04 19:40:00 +0300

    feat: add report model package

---
+++ report_model/__init__.py
@@ -0,0 +1,8 @@
+from .models import ReportDocument, ReportSection, ReportTable, ReportFinding
+
+__all__ = [
+    "ReportDocument",
+    "ReportSection",
+    "ReportTable",
+    "ReportFinding",
+]
